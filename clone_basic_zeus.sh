db521b7703d4569554202863c9fd81f9c7927436
ANUSH MADATHUMKARA <Anush.4376@gmail.com>
2023-12-03 14:05:04 +0530

    Add New kernel

--- clone_basic_zeus.sh
+++ clone_basic_zeus.sh
@@ -13,6 +13,14 @@ git clone https://gitlab.com/zeus-junk-yard/android_vendor_xiaomi_sm8450-common.
 
 git clone https://gitlab.com/zeus-junk-yard/android_vendor_xiaomi_zeus.git -b 13 vendor/xiaomi/zeus
 
+echo "Cloning Kernel"
+
+git clone git@github.com:Zeus-Junk-Yard/android_kernel_xiaomi_sm8450.git -b lineage-20 kernel/xiaomi/sm8450
+
+git clone git@github.com:Zeus-Junk-Yard/android_kernel_xiaomi_sm8450-modules.git -b lineage-20 kernel/xiaomi/sm8450-modules
+
+git clone git@github.com:Zeus-Junk-Yard/android_kernel_xiaomi_sm8450-devicetrees.git -b lineage-20 kernel/xiaomi/sm8450-devicetrees
+
 echo "Cloning MIUI Camera"
 
 git clone https://gitlab.com/zeus-junk-yard/android_vendor_xiaomi_miuicamera-zeus.git -b 13 vendor/xiaomi/miuicamera-zeus
