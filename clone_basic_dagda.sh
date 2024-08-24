dc9b54da1a896cc7c289f72c3416c407d4e53cb7
ANUSH MADATHUMKARA <Anush.4376@gmail.com>
2024-08-25 00:15:04 +0530

    Update clone_basic_zeus.sh

--- clone_basic_dagda.sh
+++ clone_basic_dagda.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Cloning device stuffs"
 
-git clone git@github.com:Zeus-Junk-Yard/android_device_xiaomi_dagda.git -b 14 device/xiaomi/dagda
+git clone git@github.com:Zeus-Junk-Yard/android_device_xiaomi_zeus.git -b 14 device/xiaomi/zeus
 
 git clone git@github.com:Zeus-Junk-Yard/android_device_xiaomi_sm8450-common.git -b 14 device/xiaomi/sm8450-common
 
@@ -11,7 +11,7 @@ git clone git@github.com:Zeus-Junk-Yard/android_hardware_xiaomi.git -b 14 hardwa
 
 git clone  git@git-ssh.mainlining.org:anush02198/proprietary_vendor_xiaomi_sm8450-common.git -b 14 vendor/xiaomi/sm8450-common
 
-git clone  git@git-ssh.mainlining.org:anush02198/proprietary_vendor_xiaomi_dagda.git -b 13 vendor/xiaomi/dagda
+git clone  git@git-ssh.mainlining.org:anush02198/proprietary_vendor_xiaomi_zeus.git -b 13 vendor/xiaomi/zeus
 
 echo "Cloning Kernel"
 
@@ -23,7 +23,7 @@ git clone  git@github.com:Zeus-Junk-Yard/android_kernel_xiaomi_sm8450-devicetree
 
 echo "Cloning MIUI Camera"
 
-git clone git@git-ssh.mainlining.org:anush02198/android_vendor_xiaomi_miuicamera-dagda.git -b 14 vendor/xiaomi/miuicamera-dagda
+git clone git@git-ssh.mainlining.org:anush02198/android_vendor_xiaomi_miuicamera-zeus.git -b 14 vendor/xiaomi/miuicamera-zeus
 
-git clone git@github.com:Zeus-Junk-Yard/android_device_xiaomi_miuicamera-dagda.git -b 14 device/xiaomi/miuicamera-dagda
+git clone git@github.com:Zeus-Junk-Yard/android_device_xiaomi_miuicamera-zeus.git -b 14 device/xiaomi/miuicamera-zeus
 
